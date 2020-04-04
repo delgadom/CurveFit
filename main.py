a9b4ee3d687154ba1b532d8c4dfa3d960c54678d
Ayush Gupta <7ayushgupta@gmail.com>
2020-04-04 14:24:02 +0530

    Reset dataframe index after filter

--- main.py
+++ main.py
@@ -12,15 +12,20 @@ def return_se_as_f_of_t(t):
     return np.random.normal(scale=0.01)
 
 dataset = pd.read_csv('dataset_for_CurveFit.csv')
-# dataset["Date"] = pd.to_datetime(dataset["Date"], infer_datetime_format=True, utc=True).astype(np.int64)
-# dataset.sort_values(by=["Date"])
-
-dataset["SE"] = np.random.normal(scale=0.01, size=dataset.shape[0])
 dataset = dataset[dataset["State/UnionTerritory"] == "Kerala"]
+dataset = dataset.reset_index(drop=True)
 print(dataset.columns)
 print(dataset)
+print(dataset["Deaths"])
+print(dataset["DateI"])
+# dataset["Date"] = pd.to_datetime(dataset["Date"], infer_datetime_format=True, utc=True).astype(np.int64)
+dataset.sort_values(by=["DateI"])
+
+dataset["SE"] = np.random.normal(scale=0.01, size=dataset.shape[0])
+
+
 model = BasicModel(all_data=dataset, col_t="DateI", col_obs="Deaths", col_group="State/UnionTerritory",
-                 col_obs_compare="Confirmed", all_cov_names=["DaysCovariate", "DaysCovariate", "DaysCovariate"], fun=expit, predict_space=expit, fit_dict={'fe_init': np.random.normal(scale=0.5, size=3)},  basic_model_dict={'col_obs_se': "SE", 'col_covs': [["DaysCovariate"], ["DaysCovariate"], ["DaysCovariate"]], 'param_names': ['alphalink', 'betalink', 'gammalink'], 'link_fun': [exponential, identity, exponential], 'var_link_fun': [exponential, identity, exponential]}, obs_se_func=return_se_as_f_of_t)
+                 col_obs_compare="Deaths", all_cov_names=["DaysCovariate", "DaysCovariate", "DaysCovariate"], fun=expit, predict_space=expit, fit_dict={'fe_init': np.random.normal(scale=0.5, size=3)},  basic_model_dict={'col_obs_se': "SE", 'col_covs': [["DaysCovariate"], ["DaysCovariate"], ["DaysCovariate"]], 'param_names': ['alphalink', 'betalink', 'gammalink'], 'link_fun': [exponential, identity, exponential], 'var_link_fun': [exponential, identity, exponential]}, obs_se_func=return_se_as_f_of_t)
 model.setup_pipeline()
 
 print("Model setup. Running fit...")
@@ -33,10 +38,11 @@ with open('model.pkl', 'wb') as f:
 
 print("Model saved.")
 
-predictions = model.predict(np.linspace(64, 120, num=120-64), predict_space=expit, predict_group="Kerala")
+predictions = model.predict(np.linspace(0, 63, num=63), predict_space=expit, predict_group="Kerala")
 
 print(predictions)
 plt.plot(predictions)
+plt.plot(dataset["Deaths"])
 plt.show()
 
 
